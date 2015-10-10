adf24b44012c944c49f9f4bf05bce210ae023569
Payam Ahangar <payam.ahangar@gmail.com>
2015-10-09 21:54:39 -0400

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_scope :user do
   get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root 'pages#home'
+  root 'pins#index'
   get 'about' => 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
