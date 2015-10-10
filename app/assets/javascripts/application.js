1d85a14aee0b551c0b53114c8e10c53c2540f576
Payam Ahangar <payam.ahangar@gmail.com>
2015-10-10 15:07:04 -0400

    final modifications to fix masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require jquery_ujs
+//= require bootstrap
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
 //= require_tree .
